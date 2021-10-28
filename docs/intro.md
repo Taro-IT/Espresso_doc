692a497674bddd08ac30c26ce250a4bce6ff3149
JoseCarlosPa <josecarlospas1@gmail.com>
2021-10-28 18:15:17 -0500

    Configuracion completa

--- docs/intro.md
+++ docs/intro.md
@@ -1,35 +1,43 @@
 ---
+id: intro
+slug: /
 sidebar_position: 1
 ---
 
-# Tutorial Intro
+# ¿Quiénes somos?
 
-Let's discover **Docusaurus in less than 5 minutes**.
+## Misión
 
-## Getting Started
+Construir soluciones de calidad que impacten positivamente en nuestros socios.
 
-Get started by **creating a new site**.
+## Visión
 
-Or **try Docusaurus immediately** with **[docusaurus.new](https://docusaurus.new)**.
+Revolucionar la industria del software desarrollando proyectos que se apeguen a los más altos estándares de calidad y que a su vez impacten de manera social y responsable.
 
-## Generate a new site
 
-Generate a new Docusaurus site using the **classic template**:
+## Objetivos
 
-```shell
-npm init docusaurus@latest my-website classic
-```
+1.-Entregar antes del 3 de diciembre del 2021 los proyectos administrados por Taro en costo, alcance y calidad para que satisfagan las necesidades de los socios y así se cumplan los objetivos acordados.
 
-## Start your site
+2.-Mejorar al menos un proceso interno en cada una de las iteraciones que se realicen para alcanzar mínimamente el nivel de certificación CMMI 2 + 7 áreas del 3 para el final del semestre.
 
-Run the development server:
+3.-Que los integrantes del Departamento Taro logren un desarrollo personal íntegro, y que logren como mínimo un nivel "Jedi" en todas las competencias de la malla de evaluación del Camino del Jedi, y al menos dos competencias en nivel "Maestro Jedi", garantizando el crecimiento de nuestros  integrantes, para la última evaluación de esta malla.
 
-```shell
-cd my-website
 
-npx docusaurus start
-```
+## Valores
 
-Your site starts at `http://localhost:3000`.
 
-Open `docs/intro.md` and edit some lines: the site **reloads automatically** and display your changes.
+**Aprendizaje:** Velar por el crecimiento de todos en el departamento reconociendo que la situación de cada miembro es diferente.
+
+
+**Empatía:** Reconocer a los miembros del departamento como personas, y considerar los sentimientos y puntos de vista de todos.
+
+
+**Integridad:** Comprometerse a hacer todo aquello que considera bueno, sin afectar los intereses de todo el departamento.
+
+
+**Responsabilidad:** Completar el trabajo asignado con calidad y manejar las consecuencias de las decisiones tomadas.
+
+
+**Transparencia:** Ser honesto en la comunicación con el cliente, así como con los demás miembros del departamento.
+
--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,118 +1,59 @@
-// @ts-check
-// Note: type annotations allow type checking and IDEs autocompletion
-
 const lightCodeTheme = require('prism-react-renderer/themes/github');
 const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
-/** @type {import('@docusaurus/types').Config} */
-const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
-  url: 'https://your-docusaurus-test-site.com',
-  baseUrl: '/',
+/** @type {import('@docusaurus/types').DocusaurusConfig} */
+module.exports = {
+  title: 'Espresso',
+  tagline: 'IT Department',
+  url: 'https://taro-it.github.io',
+  baseUrl: '/docs/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
-
+  organizationName: 'Espresso',
+  projectName: 'docs',
+  themeConfig: {
+    navbar: {
+      logo: {
+        alt: 'Espresso logo',
+        src: '/docs/img/default-monochrome.svg',
+        srcDark: '/docs/img/default-monochrome-white.svg',
+      },
+      items: [
+        {
+          href: 'https://github.com/Taro-IT/docs',
+          label: 'GitHub',
+          position: 'right',
+        },
+      ],
+    },
+    footer: {
+      style: 'dark',
+      links: [
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} Taro, Inc. Built with Docusaurus.`,
+    },
+    prism: {
+      theme: lightCodeTheme,
+      darkTheme: darkCodeTheme,
+    },
+  },
   presets: [
     [
       '@docusaurus/preset-classic',
-      /** @type {import('@docusaurus/preset-classic').Options} */
-      ({
+      {
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
+          routeBasePath: '/',
           // Please change this to your repo.
-          editUrl: 'https://github.com/facebook/docusaurus/edit/main/website/',
-        },
-        blog: {
-          showReadingTime: true,
-          // Please change this to your repo.
+          remarkPlugins: [require('mdx-mermaid')],
           editUrl:
-            'https://github.com/facebook/docusaurus/edit/main/website/blog/',
+            'https://github.com/Taro-IT/docs/edit/main/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
         },
-      }),
+      },
     ],
   ],
-
-  themeConfig:
-    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
-    ({
-      navbar: {
-        title: 'My Site',
-        logo: {
-          alt: 'My Site Logo',
-          src: 'img/logo.svg',
-        },
-        items: [
-          {
-            type: 'doc',
-            docId: 'intro',
-            position: 'left',
-            label: 'Tutorial',
-          },
-          {to: '/blog', label: 'Blog', position: 'left'},
-          {
-            href: 'https://github.com/facebook/docusaurus',
-            label: 'GitHub',
-            position: 'right',
-          },
-        ],
-      },
-      footer: {
-        style: 'dark',
-        links: [
-          {
-            title: 'Docs',
-            items: [
-              {
-                label: 'Tutorial',
-                to: '/docs/intro',
-              },
-            ],
-          },
-          {
-            title: 'Community',
-            items: [
-              {
-                label: 'Stack Overflow',
-                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-              },
-              {
-                label: 'Discord',
-                href: 'https://discordapp.com/invite/docusaurus',
-              },
-              {
-                label: 'Twitter',
-                href: 'https://twitter.com/docusaurus',
-              },
-            ],
-          },
-          {
-            title: 'More',
-            items: [
-              {
-                label: 'Blog',
-                to: '/blog',
-              },
-              {
-                label: 'GitHub',
-                href: 'https://github.com/facebook/docusaurus',
-              },
-            ],
-          },
-        ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
-      },
-      prism: {
-        theme: lightCodeTheme,
-        darkTheme: darkCodeTheme,
-      },
-    }),
 };
-
-module.exports = config;
